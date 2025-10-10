2b3adb84393c16d4f8e6de5c5d72c19c7cc1e6c5
brayanrojas2019 <brayanrojas2112@gmail.com>
2025-10-09 22:48:50 -0500

    implementación de characters en episodios

--- ProyectoApi/src/app/core/Interfaces/IEpisodes.ts
+++ ProyectoApi/src/app/core/Interfaces/IEpisodes.ts
@@ -1,9 +1,12 @@
+import { Character } from './ICharacter';
+
 export interface Episode {
   id: number;
   name: string;
   air_date: string;
   episode: string;
   characters: string[];
+  charactersData?: Character[]; // Array para almacenar los datos de los personajes
   url: string;
   created: string;
 }
--- ProyectoApi/src/app/pages/episodes/episodes.ts
+++ ProyectoApi/src/app/pages/episodes/episodes.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RickAndMortyService } from '../../core/services/rick-and-morty.service';
 import { Episode } from '../../core/Interfaces/IEpisodes';
 import { FormsModule } from '@angular/forms';
+import { Character } from '../../core/Interfaces/ICharacter';
 
 @Component({
   selector: 'app-episodes',
@@ -36,6 +37,12 @@ export class Episodes implements OnInit {
     }
   }
 
+  //Función para extraer el ID del personaje desde la URL
+  getCharacterId(url: string): number {
+    const match = url.match(/\/(\d+)$/); // Captura el número al final de la URL
+    return match ? parseInt(match[1], 10) : 0;
+  }
+
   private fetch(reset: boolean) {
     this.loading = true;
     this.errorMsg = '';
@@ -43,7 +50,27 @@ export class Episodes implements OnInit {
       .subscribe({
         next: (res) => {
           this.pages = res.info.pages ?? 1;
-          this.episodes = reset ? res.results : [...this.episodes, ...res.results];
+          const episodes = reset ? res.results : [...this.episodes, ...res.results];
+          
+          // Para cada episodio, obtener los personajes
+          episodes.forEach(episode => {
+            episode.charactersData = [];
+            if (episode.characters && episode.characters.length > 0) {
+              episode.characters.forEach(characterUrl => {
+                const characterId = this.getCharacterId(characterUrl);
+                if (characterId) {
+                  this.api.getCharacterbyId(characterId).subscribe({
+                    next: (character: any) => {
+                      if (!episode.charactersData) episode.charactersData = [];
+                      episode.charactersData.push(character);
+                    }
+                  });
+                }
+              });
+            }
+          });
+          
+          this.episodes = episodes;
           this.loading = false;
         },
         error: (err) => {
